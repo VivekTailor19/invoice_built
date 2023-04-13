9f8e1fc45f09b684a6cc6adff85354978584ce62
VIVEK <tailorvivekgec19@gmail.com>
2023-04-13 07:09:55 +0530

    invoice generate

--- lib/main.dart
+++ lib/main.dart
@@ -5,16 +5,12 @@ import 'package:invoice_built/client/client.dart';
 import 'package:invoice_built/invoice/invoice_view.dart';
 import 'package:invoice_built/items/additem.dart';
 
-
-import 'invoice_data.dart';
-
-
 void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
     routes: {
       "/": (context) => Splash(),
-      "data":(context) => Invoice_Datas(),
+
       "business":(context) => Business(),
       "client":(context) => Clients(),
       "additem":(context) => AddItems(),
