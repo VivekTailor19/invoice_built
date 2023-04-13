906cf1951cda02eee097f88bbeff65a037b1d1c7
VIVEK <tailorvivekgec19@gmail.com>
2023-04-13 20:40:35 +0530

    invoice generate using flutter

--- lib/items/additem.dart
+++ lib/items/additem.dart
@@ -12,16 +12,14 @@ class AddItems extends StatefulWidget {
 
 class _AddItemsState extends State<AddItems> {
 
-  TextEditingController iid = TextEditingController();
-  TextEditingController iname = TextEditingController();
-  TextEditingController idata = TextEditingController();
-  TextEditingController iprice = TextEditingController();
-  TextEditingController iqty = TextEditingController();
-  TextEditingController itotal = TextEditingController();
+  TextEditingController pid = TextEditingController();
+  TextEditingController pname = TextEditingController();
+  TextEditingController pdata = TextEditingController();
+  TextEditingController pprice = TextEditingController();
+  TextEditingController pqty = TextEditingController();
+  TextEditingController ptotal = TextEditingController();
 
-  int total = 0;
-
-  List<Items_Modal> products = [];
+  List<Items_Modal> products = [Items_Modal(iid: '101',iname: 'Happiness',idata: '1 cr',iprice: '0',iqty: '1000',itotal: '0')];
 
 
 
@@ -36,7 +34,7 @@ class _AddItemsState extends State<AddItems> {
           children: [
             SizedBox(height: 15),
             ElevatedButton(onPressed: () {
-              alertproduct();
+              alertAddProduct();
             },child: Text("Add"),),
             SizedBox(height: 15),
             Expanded(
@@ -55,34 +53,69 @@ class _AddItemsState extends State<AddItems> {
     );
   }
 
-  void alertproduct()
+  void alertAddProduct()
   {
     showDialog(context: context, builder: (context) {
       return AlertDialog(
         title: Text("Add Product"),
         content: SingleChildScrollView(
           child: Column(mainAxisSize: MainAxisSize.min,
-            children: [
-              Info(hint: "Product Id....",kboard: TextInputType.text,controller: iid),
-              Info(hint: "Product Name....",kboard: TextInputType.text,controller: iname),
-              Info(hint: "Product Description....",kboard: TextInputType.text,controller: idata),
+          children: [
+              Info(hint: "Product Id....",kboard: TextInputType.text,controller: pid),
+              Info(hint: "Product Name....",kboard: TextInputType.text,controller: pname),
+              Info(hint: "Description....",kboard: TextInputType.text,controller: pdata),
               Row(mainAxisAlignment: MainAxisAlignment.spaceAround,
                 children: [
-                  Container(width: 115,child: Info(hint: "Price...",kboard: TextInputType.number,controller: iprice)),
-                  Container(width: 115,child: Info(hint: "Qty...",kboard: TextInputType.number,controller: iqty))
+                  Container(width: 115,child: Info(hint: "Price...",kboard: TextInputType.number,controller: pprice)),
+                  Container(width: 115,child: Info(hint: "Qty...",kboard: TextInputType.number,controller: pqty))
                 ],),
-              Info(hint: "Total Amount",kboard: TextInputType.number,controller: itotal),
+              TextFormField(
+                onChanged: (value) {
+                  setState(() {
+                    int total = 0;
+                    total = int.parse(pprice.text) * int.parse(pqty.text);
+                    print(total);
+
+                    ptotal.text = total.toString();
+                    value = ptotal.text;
+                  });
+                },
+                controller: ptotal,
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.blueGrey.shade800,fontSize: 20),
+                scrollPhysics: BouncingScrollPhysics(),
+                decoration: InputDecoration(
+                  // hintText: "Total Amount",
+                  // hintStyle: TextStyle(fontSize: 20,color: Colors.blueGrey.shade100,wordSpacing: 3),
+                  filled: true,
+                  fillColor: Colors.white,
+                  enabled: true,
+                  enabledBorder: OutlineInputBorder(borderRadius: BorderRadius.all(Radius.circular(20)),borderSide: BorderSide(color: Colors.indigo.shade200)),
+                  focusedBorder: OutlineInputBorder(borderRadius: BorderRadius.all(Radius.circular(20)),borderSide: BorderSide(color: Colors.indigo.shade700)),
+                ),
+              ),
+
               SizedBox(height: 15),
+
               ElevatedButton(onPressed: () {
 
-                Items_Modal item = Items_Modal(iid: iid.text,iname: iname.text,idata: idata.text,iprice: iprice.text,iqty: iqty.text,itotal: itotal.text);
+
                 setState(() {
+                  Items_Modal item = Items_Modal(iid: pid.text,iname: pname.text,idata: pdata.text,iprice: pprice.text,iqty: pqty.text,itotal: ptotal.text);
                   products.add(item);
+                  pid.text = '';
+                  pname.text = '';
+                  pdata.text= '';
+                  pprice.text = '';
+                  pqty.text = '';
+                  ptotal.text = '';
+
                 });
 
 
                 Navigator.pop(context);
-                ScaffoldMessenger.of(context)!.showSnackBar(SnackBar(behavior:SnackBarBehavior.floating,content: Text("Thanks for Adding Product in List"),backgroundColor: Colors.green.shade400,));
+                ScaffoldMessenger.of(context).showSnackBar(SnackBar(behavior:SnackBarBehavior.floating,content: Text("Thanks for Adding Product in List"),backgroundColor: Colors.green.shade400,));
+
               },child: Text("Add"),),
               SizedBox(height: 15),
 
@@ -108,14 +141,15 @@ class _AddItemsState extends State<AddItems> {
                 padding: const EdgeInsets.symmetric(horizontal: 10.0),
                 child: Row(
                 children: [
+                  Text("${im.iid}",style: TextStyle(fontSize: 17,color: Color(0xff4474fa)),),
+                  SizedBox(width: 10),
                   Column(
                     mainAxisAlignment: MainAxisAlignment.spaceAround,
                     children: [
                       Text("${im.iname}",style: TextStyle(fontSize: 15,color: Color(0xff4474fa)),),
-                      Text("${im.idata}",style: TextStyle(fontSize: 12,color: Color(0xffB6C9FD)),),
+                      Text("${im.idata}",style: TextStyle(fontSize: 13,color: Color(0xffB6C9FD)),),
                     ],
                   ),
-                  SizedBox(width: 15),
                   Spacer(),
                   Column(mainAxisAlignment: MainAxisAlignment.spaceAround,
                     children: [
