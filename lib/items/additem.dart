6be9131c7bcef4553e7aa8740af36e2eb1af38c2
Vivek <tailorvivekgec19@gmail.com>
2023-04-13 10:57:03 +0530

    invoice generate using flutter

--- lib/items/additem.dart
+++ lib/items/additem.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:invoice_built/homescreen.dart';
 
 class AddItems extends StatefulWidget {
   const AddItems({Key? key}) : super(key: key);
@@ -8,11 +9,40 @@ class AddItems extends StatefulWidget {
 }
 
 class _AddItemsState extends State<AddItems> {
+
+  TextEditingController iid = TextEditingController();
+  TextEditingController iname = TextEditingController();
+  TextEditingController iprice = TextEditingController();
+  TextEditingController iqty = TextEditingController();
+
+  int total = 0;
+
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(backgroundColor: Colors.blueGrey.shade500,
         title: Text("Products",style: TextStyle(fontSize: 30,color: Colors.white)),),
+
+      body: Column(
+        children: [
+          Info(hint: "Product Id....",kboard: TextInputType.text,controller: iname),
+          Info(hint: "Product Name....",kboard: TextInputType.text,controller: iname),
+          Info(hint: "Product Description....",kboard: TextInputType.text,controller: iname),
+          Row(mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+            Container(width: 150,child: Info(hint: "Price....",kboard: TextInputType.text,controller: iname)),
+            Container(width: 150,child: Info(hint: "Quantity....",kboard: TextInputType.text,controller: iname))
+          ],),
+          Container(width: 150,child: Text("Hello World")),
+
+
+        ],
+      ),
     );
   }
 }
+
+// Items
+// Name, Description, Price, Quantity, Total Price
---
+++ lib/items/item_modal.dart
@@ -0,0 +1,6 @@
+class Items_Modal
+{
+  String? iid,iname,idata,iprice,iqty,itotal,itax;
+
+  Items_Modal({this.iid,this.iname, this.idata,this.iprice, this.iqty, this.itotal,});
+}
